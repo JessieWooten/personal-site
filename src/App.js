73502eea79f21a5cb1b80ccba7f3c0803042728b
Jessie Wooten <jessiewooten@Jessies-MacBook-Pro.local>
2020-05-20 16:06:22 -0500

    Feat: Add Footer

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navigation from "./components/Navigation";
 import Hero from "./components/Hero";
 import About from "./components/About";
 import Portfolio from "./components/Portfolio";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Hero />
       <About />
       <Portfolio />
+      <Footer />
     </div>
   );
 }
