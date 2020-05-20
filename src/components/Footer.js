8a00e445943cedd5755881598ac861d0380f1f58
Jessie Wooten <jessiewooten@Jessies-MacBook-Pro.local>
2020-05-20 16:06:52 -0500

    enhance: Add footer copy

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,13 @@
 import React from "react";
 
 const Footer = () => {
-  return <footer ></footer>;
+  return (
+    <footer className="w-full black-bg">
+      <p className="text-center white p-6 font-thin text-sm">
+        © Jessie Wooten {new Date().getFullYear()}
+      </p>
+    </footer>
+  );
 };
 
 export default Footer;
