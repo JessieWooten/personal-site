d885dfbe68975ed5f8de7ca7372d394b0c226528
Jessie Wooten <jessiewooten@Jessies-MacBook-Pro.local>
2020-05-24 18:24:02 -0500

    feat: build out portfolio card

---
+++ src/components/Card.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Card = ({
+  title,
+  backgroundImg,
+  foregroundImg,
+  alt,
+  description,
+  onClick,
+}) => {
+  return (
+    <div
+      className="w-full overflow-hidden card mr-2"
+      onClick={onClick}
+      style={{ backgroundImage: `url(${backgroundImg}` }}
+    >
+      {/* Foreground image */}
+      <img src={foregroundImg} className="w-4/5 foreground-image" />
+      <div className="p-3 info flex justify-center flex-col items-center">
+        <h3 className="text-sm font-bold">{title}</h3>
+        <p className="text-xs">{description}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
