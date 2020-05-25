b7c2f5c11dc02448a486de175c14bc06a9bac53e
Jessie Wooten <jessiewooten@Jessies-MacBook-Pro.local>
2020-05-24 19:45:34 -0500

    feat: add routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import HomePage from "./pages/Home";
+import SupportPage from "./pages/Support";
 
 function App() {
   return (
     <div className="App">
-      <HomePage />
+      <Router>
+        <Switch>
+          <Route path="/support">
+            <SupportPage />
+          </Route>
+          <Route path="/">
+            <HomePage />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
