cd59599b853562beb533c4e5dc5296467f2757e7
Jessie Wooten <jessiewooten@Jessies-MacBook-Pro.local>
2020-05-25 12:16:41 -0500

    enhance: add image dir as context. add tech prop. handle bg images

--- src/components/Card.js
+++ src/components/Card.js
@@ -6,23 +6,30 @@ const Card = ({
   foregroundImg,
   alt,
   description,
+  tech,
   onClick,
 }) => {
+  const imagesDir = require.context("../assets", true);
   return (
     <div
       className="w-full overflow-hidden card mr-2"
       onClick={onClick}
-      style={{ backgroundImage: `url(${backgroundImg}` }}
+      style={{
+        background: backgroundImg.includes("http")
+          ? `url(${backgroundImg}`
+          : backgroundImg,
+      }}
     >
       {/* Foreground image */}
       <img
-        src={foregroundImg}
+        src={imagesDir(`./${foregroundImg}`)}
         alt={alt || ""}
-        className="w-4/5 foreground-image"
+        className="w-2/4 foreground-image"
       />
       <div className="p-3 info flex justify-center flex-col items-center">
         <h3 className="text-sm font-bold">{title}</h3>
         <p className="text-xs">{description}</p>
+        <p className="text-xs">Technologies: {tech}</p>
       </div>
     </div>
   );
