43108c98b27f6e84c05d9343da6ce80257e7bfff
Jessie Wooten <jessiewooten@Jessies-MacBook-Pro.local>
2020-05-26 10:45:20 -0500

    enhance: only apply animation when scrolling down

--- src/components/FadeIn.js
+++ src/components/FadeIn.js
@@ -1,16 +1,19 @@
 import React, { useRef } from "react";
 
 const FadeInSection = (props) => {
-  const [isVisible, setVisible] = React.useState(true);
+  const [isVisible, setVisible] = React.useState(false);
   const domRef = useRef();
   React.useEffect(() => {
     const currentRef = domRef.current;
     const observer = new IntersectionObserver((entries) => {
-      entries.forEach((entry) => setVisible(entry.isIntersecting));
+      entries.forEach((entry) => {
+        // only when scrolling down
+        if (entry.boundingClientRect.top > 0) setVisible(entry.isIntersecting);
+      });
     });
     observer.observe(currentRef);
     return () => observer.unobserve(currentRef);
-  }, []);
+  }, [isVisible]);
   return (
     <div
       className={`fade-in-section ${isVisible ? "is-visible" : ""}`}
