3022c9a4e936c8946f239ac49864fedbec868c6b
Jessie Wooten <jessiewooten@Jessies-MacBook-Pro.local>
2020-05-26 12:20:14 -0500

    feat: add Band Director privacy policy

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import HomePage from "./pages/Home";
 import SupportPage from "./pages/Support";
+import BandDirectorPrivacy from "./pages/privacy/BandDirector";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Router>
         <Switch>
           <Route path="/support" component={SupportPage} />
+          <Route path="/privacy/banddirector" component={BandDirectorPrivacy} />
           <Route path="/" component={HomePage} />
         </Switch>
       </Router>
