b834da4e8757bfe9e4936c95ed590fce99e3377a
Jessie Wooten <jessiewooten@Jessies-MacBook-Pro.local>
2020-05-26 11:02:16 -0500

    stlye: remove unused navigation and about components

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,12 @@
 import React from "react";
-import Navigation from "../components/Navigation";
 import Hero from "../components/home/Hero";
-// import About from "../components/home/About";
 import Portfolio from "../components/home/Portfolio";
 import ContactMe from "../components/home/ContactMe";
 import Footer from "../components/Footer";
 
 const HomePage = () => (
   <div className="App hero-gradient-background">
-    <Navigation />
     <Hero />
-    {/* <About /> */}
     <Portfolio />
     <ContactMe />
     <Footer />
