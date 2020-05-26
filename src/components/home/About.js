ab8be3626078a04f4ebaab74da3b271c5db8f940
Jessie Wooten <jessiewooten@Jessies-MacBook-Pro.local>
2020-05-26 10:52:47 -0500

    wip: bio section

--- src/components/home/About.js
+++ src/components/home/About.js
@@ -9,23 +9,24 @@ const About = () => (
   //   className="w-50 py-10 px-8 sm:px-20 lg:px-56 flex items-center justify-center text-center"
   //   style={aboutStyle}
   // >
-  <Container className="pt-56 px-8 white-bg relative">
-    <Row className="w-full">
-      <h2 className="title pb-8">About Me</h2>
-    </Row>
+  <Container className="pt-32 text-center px-8 white-bg relative">
     <FadeInScroll>
       <Row>
-        <Col className="pr-8 w-full md:w-1/3">
+        {/* <Col className="pr-8 w-full md:w-1/3">
           <ImageWrapper className="m-auto md:m-0">
             <img className="me" alt="me" src={JessiePic} />
           </ImageWrapper>
-        </Col>
-        <Col className="w-full md:w-2/3">
-          <p className="text">
-            I am a self-taught developer from Nashville, TN with a passion for
-            solving unique problems through web technologies. I have spent the
-            last 4 years building complex single page web applications, hybrid
-            mobile applications, and responsive web sites. I love what I do!
+        </Col> */}
+        <Col className="w-full bg-white shadow-lg rounded px-8 pt-6 pb-8 mb-4 slide-in-bottom-with-fade animate600ms">
+          <h3 className="black">Hey, my name is Jessie </h3>
+          <p className="text black px-8">
+            my name is Jessie and I'm a full stack developer from Nashville, TN.
+            I'm self-taught so I love learning and I'm always looking for that
+            new hotness add to the toolbelt! I have spent the last 4 years
+            building complex single page web applications, hybrid mobile
+            applications, and responsive web sites.
+            <br />
+            What can I say, <em>I love what I do!</em>
           </p>
         </Col>
       </Row>
