06cb7cef2e499b96eebe11496883640895c18a03
Jessie Wooten <jessiewooten@Jessies-MacBook-Pro.local>
2020-05-27 16:56:51 -0500

    feat: add technology icons for project

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,12 +1,39 @@
 import React from "react";
 
+function getTechImage(tech) {
+  switch (tech) {
+    case "vue":
+      return { className: "w-8 mr-2", img: "vue.png", title: "Vue.js" };
+    case "react":
+      return { className: "w-10 mr-2", img: "react.png", title: "React.js" };
+    case "typescript":
+      return {
+        className: "w-8 mr-2",
+        img: "typescript.png",
+        title: "TypeScript",
+      };
+    case "node":
+      return { className: "w-8 mr-2", img: "node.png", title: "Node.js" };
+    case "swift":
+      return { className: "w-8 mr-2", img: "swift.png", title: "Swift" };
+    case "java":
+      return { className: "w-8 mr-2", img: "java.png", title: "Java" };
+    case "express":
+      return { className: "h-6 mr-2", img: "express.png", title: "Express.js" };
+    case "mysql":
+      return { className: "w-8 mr-2 flex-1", img: "mysql.png", title: "MySQL" };
+    default:
+      return { className: "w-8 mr-2", img: "", title: "" };
+  }
+}
+
 const Card = ({
   title,
-  backgroundImg,
+  background,
   foregroundImg,
   alt,
-  description,
-  tech,
+  summary,
+  techs,
   onClick,
 }) => {
   const imagesDir = require.context("../assets", true);
@@ -15,9 +42,9 @@ const Card = ({
       className="w-full overflow-hidden card mr-2"
       onClick={onClick}
       style={{
-        background: backgroundImg.includes("http")
-          ? `url(${backgroundImg}`
-          : backgroundImg,
+        background: background.includes("http")
+          ? `url(${background}`
+          : background,
       }}
     >
       {/* Foreground image */}
@@ -28,8 +55,21 @@ const Card = ({
       />
       <div className="p-3 info flex justify-center flex-col items-center">
         <h3 className="text-sm font-bold">{title}</h3>
-        <p className="text-xs">{description}</p>
-        <p className="text-xs">Technologies: {tech}</p>
+        <p className="text-xs">{summary}</p>
+        <div className="flex items-center justify-center flex-wrap mt-2">
+          {techs.map((t, i) => {
+            const tech = getTechImage(t.toLowerCase());
+            return (
+              <img
+                key={i}
+                title={tech.title}
+                className={tech.className}
+                src={imagesDir(`./logos/${tech.img}`)}
+                alt={tech.title}
+              />
+            );
+          })}
+        </div>
       </div>
     </div>
   );
